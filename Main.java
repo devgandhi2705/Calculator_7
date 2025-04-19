1534a7c396b01515e064da94d8ce90f9f90eb3c8
Dev gandhi <devgandhi2705@gmail.com>
2025-04-20 02:55:53 +0530

    initialize

---
+++ Main.java
@@ -0,0 +1,21 @@
+/* 
+Name: Dev gandhi
+PRN: 23070126046
+Batch: AIML A2
+*/
+
+import java.util.Scanner;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+       int choice;
+       double a, b;
+
+
+    
+
+    }
+
+}
